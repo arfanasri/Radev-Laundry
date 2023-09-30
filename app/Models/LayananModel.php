51e7c7efcebe51eefea2d1e617cc7149a05825ac
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-09-30 16:50:19 +0800

    Create LayananModel.php

---
+++ app/Models/LayananModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class LayananModel extends BaseModel
+{
+    protected $table = 'layanan';
+    protected $primaryKey = 'id_layanan';
+    protected $useAutoIncrement = true;
+    protected $allowedFields = [
+        'nama_layanan',
+        'harga',
+        'satuan',
+    ];
+
+    // Validation
+    protected $validationRules = [];
+}
