a3f39d85d007be6fa9f7951f780dab1d964b4d1d
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-09-30 16:50:31 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('api', ['namespace' => 'App\Controllers\Api'], static function ($routes) {
+    $routes->resource('layanan');
+});
