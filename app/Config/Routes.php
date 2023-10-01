657ad72eb5eba3e1867c99364e7a5356c15d3a14
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-10-01 16:11:38 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,4 +11,13 @@ $routes->group('api', ['namespace' => 'App\Controllers\Api'], static function ($
     $routes->resource('layanan');
     $routes->resource('pelanggan');
     $routes->resource('transaksi');
+    // Pesanan
+    $routes->get('pesanan/(:segment)/new', 'Pesanan::new/$1');
+    $routes->post('pesanan/(:segment)', 'Pesanan::create/$1');
+    $routes->get('pesanan/(:segment)', 'Pesanan::get/$1');
+    $routes->get('pesanan/(:segment)/(:segment)', 'Pesanan::show/$1/$2');
+    $routes->get('pesanan/(:segment)/(:segment)/edit', 'Pesanan::edit/$1/$2');
+    $routes->put('pesanan/(:segment)/(:segment)', 'Pesanan::update/$1/$2');
+    $routes->patch('pesanan/(:segment)/(:segment)', 'Pesanan::update/$1/$2');
+    $routes->delete('pesanan/(:segment)/(:segment)', 'Pesanan::delete/$1/$2');
 });
