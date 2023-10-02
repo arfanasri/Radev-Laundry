e2c9a0cee165c454ddc245b7b1c0e162d2942a64
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-10-02 13:21:09 +0800

    Create contoh_isi.php

---
+++ app/Views/contoh_isi.php
@@ -0,0 +1,11 @@
+<?= $this->extend("template/main") ?>
+
+<?= $this->section("main") ?>
+<h1>Hello World</h1>
+<?= $this->endSection() ?>
+
+<?= $this->section("css") ?>
+<?= $this->endSection() ?>
+
+<?= $this->section("js") ?>
+<?= $this->endSection() ?>
