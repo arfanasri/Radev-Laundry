9fb49d80247d08340e1918b766bf961f9f7a9acd
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-10-04 21:44:41 +0800

    Update ra_helper.php

--- app/Helpers/ra_helper.php
+++ app/Helpers/ra_helper.php
@@ -1,1 +1,17 @@
-<?php ?>
+<?php
+
+function menu(string $menu, string|array $pilihan, string $kembali = "active"): string
+{
+    if (!is_array($pilihan)) {
+        $pilihan = [$pilihan];
+    }
+
+    foreach ($pilihan as $pilih) {
+        if ($menu == $pilih)
+            return $kembali;
+    }
+
+    return "";
+}
+
+?>
