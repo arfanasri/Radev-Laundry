24a3536ae0b637a57314868b48ae893649f4556b
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-10-06 11:36:38 +0800

    Update main.php

--- app/Views/template/main.php
+++ app/Views/template/main.php
@@ -12,6 +12,7 @@
     </title>
     <link href="<?= base_url() ?>assets/css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.31/dist/sweetalert2.min.css">
     <?= $this->renderSection("css") ?>
 </head>
 
@@ -31,10 +32,10 @@
             <?= $this->include("template/footer") ?>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.31/dist/sweetalert2.all.min.js"></script>
     <script src="<?= base_url() ?>assets/js/scripts.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <?= $this->renderSection("js") ?>
