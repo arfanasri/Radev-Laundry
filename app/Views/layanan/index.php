353795f329d45d766da1a88629b8aa6597691328
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-10-06 11:36:32 +0800

    Update index.php

--- app/Views/layanan/index.php
+++ app/Views/layanan/index.php
@@ -8,7 +8,7 @@
         <div class="float-end">
             <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#modaltampil"
                 onclick="tambahLayanan()">
-                Tambah Layanan
+                Tambah
             </button>
         </div>
     </div>
@@ -18,7 +18,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modaltampil" tabindex="-1" aria-labelledby="modaltampilLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content" id="isimodal">
         </div>
     </div>
@@ -30,10 +30,18 @@
 
 <?= $this->section("js") ?>
 <script>
+    function ready(callback) {
+        if (document.readyState != 'loading') callback();
+        else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+        else document.attachEvent('onreadystatechange', function () {
+            if (document.readyState == 'complete') callback();
+        })
+    };
+
     async function dataLayanan() {
         try {
             const response = await axios.post('<?= url_to("layanan.data") ?>');
-            $("#tampildata").html(response.data);
+            document.querySelector("#tampildata").innerHTML = response.data;
         } catch (error) {
             console.error(error);
         }
@@ -42,10 +50,41 @@
     async function tambahLayanan() {
         try {
             const response = await axios.post('<?= url_to("layanan.tambah") ?>');
-            $("#isimodal").html(response.data);
+            document.querySelector("#isimodal").innerHTML = response.data;
         } catch (error) {
             console.error(error);
         }
+    }
+
+    async function ubahLayanan(id) {
+        try {
+            const response = await axios.post('<?= site_url("layanan/ubah") ?>/' + id.toString());
+            document.querySelector("#isimodal").innerHTML = response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    function konfirmasiHapus(id) {
+        Swal.fire({
+            title: 'Apakah anda yakin ingin menghapus data ini?',
+            text: "Data yang telah dihapus tidak dapat dikembalikan",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya',
+            cancelButtonText: 'Tidak'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                hapusLayanan(id);
+                Swal.fire(
+                    'Dihapus',
+                    'Data berhasil dihapus',
+                    'success'
+                )
+            }
+        })
     }
 
     async function buatLayanan() {
@@ -54,18 +93,58 @@
             const data = ambilDataForm();
             const response = await axios.post('<?= url_to("api/layanan") ?>', data);
             setelahSimpan();
-            $("#modaltampil").modal("hide");
+            bersihkanDataForm();
             dataLayanan();
         } catch (error) {
             setelahSimpan();
+            Swal.fire({
+                position: 'top',
+                icon: 'error',
+                title: 'Terjadi Kesalahan',
+                text: JSON.stringify(error.response.data.messages),
+                showConfirmButton: false,
+                timer: 5000,
+                toast: true,
+            });
+            console.error(error);
+        }
+    }
+
+    async function perbaruiLayanan(id) {
+        try {
+            sebelumSimpan();
+            const data = ambilDataForm();
+            const response = await axios.patch('<?= url_to("api/layanan") ?>/' + id.toString(), data);
+            setelahSimpan();
+            dataLayanan();
+        } catch (error) {
+            setelahSimpan();
+            Swal.fire({
+                position: 'top',
+                icon: 'error',
+                title: 'Terjadi Kesalahan',
+                text: JSON.stringify(error.response.data.messages),
+                showConfirmButton: false,
+                timer: 5000,
+                toast: true,
+            });
+            console.error(error);
+        }
+    }
+
+    async function hapusLayanan(id) {
+        try {
+            const response = await axios.delete('<?= url_to("api/layanan") ?>/' + id.toString());
+            dataLayanan();
+        } catch (error) {
             console.error(error);
         }
     }
 
     function ambilDataForm() {
-        nama_layanan = $("#nama_layanan").value;
-        harga = $("#harga").value;
-        satuan = $("#satuan").value;
+        nama_layanan = document.querySelector("#nama_layanan").value;
+        harga = document.querySelector("#harga").value;
+        satuan = document.querySelector("#satuan").value;
         const dataForm = {
             nama_layanan: nama_layanan,
             harga: harga,
@@ -75,19 +154,28 @@
         return dataForm;
     }
 
+    function bersihkanDataForm() {
+        document.querySelector("#nama_layanan").value = "";
+        document.querySelector("#harga").value = "";
+        document.querySelector("#satuan").value = "";
+    }
+
     function sebelumSimpan() {
-        $('#tombolsimpan').attr('disable', 'disabled');
-        $('#tombolsimpan').html('<i class=" . '" fas fa-spin fa-spinner"' . "></i>');
+        document.querySelector('#tombolsimpan').setAttribute('disable', 'disabled');
+        document.querySelector('#tombolsimpan').removeAttribute('onclick');
+        document.querySelector('#tombolsimpan').innerHTML = '<i class="fas fa-spin fa-spinner"></i>';
     }
 
     function setelahSimpan() {
-        $('#tombolsimpan').removeAttr('disable');
-        $('#tombolsimpan').html('Simpan');
+        document.querySelector('#tombolsimpan').setAttribute('onclick', 'buatLayanan()');
+        document.querySelector('#tombolsimpan').removeAttribute('disable');
+        document.querySelector('#tombolsimpan').innerHTML = 'Simpan';
     }
 
-    $(document).ready(function () {
+    ready(function () {
         dataLayanan();
-    });
+    })
+
 
 
 </script>
