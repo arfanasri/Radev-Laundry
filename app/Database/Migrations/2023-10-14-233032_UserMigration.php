810b3ce3187432a1337eb0dab766ef16c8a679e4
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-10-15 13:23:00 +0800

    Create 2023-10-14-233032_UserMigration.php

---
+++ app/Database/Migrations/2023-10-14-233032_UserMigration.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UserMigration extends Migration
+{
+    public function up()
+    {
+
+        $fields = [
+            'id_user' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nama_user' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'role' => [
+                'type' => 'ENUM',
+                'constraint' => ['admin'],
+                'default' => 'admin',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'default' => NULL
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'default' => NULL
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'default' => NULL
+            ],
+        ];
+
+        $this->forge->addField($fields);
+
+        $this->forge->addPrimaryKey("id_user");
+
+        $this->forge->createTable("user");
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable("user");
+    }
+}
