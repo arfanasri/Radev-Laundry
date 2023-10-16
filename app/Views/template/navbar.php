8fd3cb7c25af562c6a4fa240a0ac0a45146d324a
Muhammad Arfan Asri <arfanasri@gmail.com>
2023-10-16 19:44:47 +0800

    Update navbar.php

--- app/Views/template/navbar.php
+++ app/Views/template/navbar.php
@@ -18,7 +18,7 @@
                 <li>
                     <hr class="dropdown-divider" />
                 </li>
-                <li><a class="dropdown-item" href="#!">Logout</a></li>
+                <li><a class="dropdown-item" href="<?= url_to("logout") ?>">Logout</a></li>
             </ul>
         </li>
     </ul>
